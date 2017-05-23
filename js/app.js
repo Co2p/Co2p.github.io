edf4b1f859f70669d0a20213d15597f28ce3276e
Co2p <go.co2p@gmail.com>
2017-05-23 19:12:35 +0200

    rebuild

---
+++ js/app.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('https://co2p.github.io/sw.js')
+  .then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
